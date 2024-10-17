d4f27111ea6f0bc0609bf21c5b96a0c581a42ae6
Ben Lucas <blucas@hiddenlayer.com>
2024-10-17 12:57:05 -0700

    fix concurrency issue. fix handling of failOnDetections input

--- task/tests/_suite.ts
+++ task/tests/_suite.ts
@@ -83,4 +83,22 @@ describe('Model Scanner task tests', function () {
       done(error); // Ensure the test case fails if there's an error
     });
   });
+
+  it('it should succeed with warnings if scanning a folder with a malicious model and failOnDetections is false', function(done: Mocha.Done) {
+    this.timeout(30000);
+
+    const tp = path.join(__dirname, 'folder_failure_without_fail_on_detections.js');
+    const tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
+
+    tr.runAsync().then(() => {
+      console.log(tr);
+      assert.equal(tr.succeeded, true, 'should have failed');
+      assert.equal(tr.warningIssues.length, 1, "should have 1 warning");
+      assert.equal(tr.errorIssues.length, 0, "should have 0 errors");
+
+      done();
+    }).catch((error) => {
+      done(error); // Ensure the test case fails if there's an error
+    });
+  });
 });
