290e205cbd083b55a0360037c475de8abd220a07
Andrew Drummond <adrummond@hiddenlayer.com>
2025-06-12 14:31:07 -0600

    INT-89 implement community scan support

--- task/index.ts
+++ task/index.ts
@@ -1,4 +1,4 @@
-import { HiddenLayerServiceClient, ScanReportV3, ResponseError, Sarif210 } from '@hiddenlayerai/hiddenlayer-sdk';
+import { HiddenLayerServiceClient, ScanReportV3, ResponseError, Sarif210, ScanJobAccessSourceEnum } from '@hiddenlayerai/hiddenlayer-sdk';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 import tl = require('azure-pipelines-task-lib/task');
 import * as fs from 'fs';
@@ -12,6 +12,8 @@ async function run() {
         const modelPath: string = tl.getInput('modelPath', true) || "";
         const failOnDetections: boolean = tl.getBoolInput('failOnDetections', false);
         const sarifFile: string = tl.getInput('sarifFile', false) || "";
+        const communityScan: string = tl.getInput('communityScan', false) || "";
+        const modelVersion: string = tl.getInput('modelVersion', false) || "";
 
         const client = HiddenLayerServiceClient.createSaaSClient(clientId, clientSecret, apiUrl);
 
@@ -29,19 +31,81 @@ async function run() {
             }
         }
 
-        const stats = fs.statSync(modelPath);
+        if (sarifFile) {
+            try {
+                const sarifFileDir = path.dirname(path.resolve(sarifFile));
+                if (!fs.existsSync(sarifFileDir)) {
+                    fs.mkdirSync(sarifFileDir, { recursive: true });
+                }
+                else {
+                    await fs.promises.access(sarifFileDir, fs.constants.W_OK);
+                }
+            } catch {
+                throw new Error(`The current user does not have permissions to write to ${path.resolve(sarifFile)}`);
+            }
+        }
+
         const splitResult = modelPath.split('/');
         if (splitResult[splitResult.length - 1] === '' ) {
             // model path was a folder, so get rid of extra element
             splitResult.pop();
         }
         const modelName: string = splitResult.pop() || 'model';
-        let results;
-        if (stats.isDirectory()) {
-            results = await client.modelScanner.scanFolder(modelName, modelPath);
+        
+        let results: ScanReportV3;
+        
+        // Handle community scanning first, similar to Python implementation
+        if (communityScan) {
+            let scanType: ScanJobAccessSourceEnum;
+            let version: string = modelVersion;
+            
+            // Map community scan types to SDK enum values
+            switch (communityScan) {
+                case 'HUGGING_FACE':
+                    scanType = ScanJobAccessSourceEnum.HuggingFace;
+                    // Default to 'main' for Hugging Face if no version specified
+                    if (!version) {
+                        version = 'main';
+                    }
+                    break;
+                case 'AWS_PRESIGNED':
+                    scanType = ScanJobAccessSourceEnum.AwsPresigned;
+                    break;
+                case 'AWS_IAM_ROLE':
+                    scanType = ScanJobAccessSourceEnum.AwsIamRole;
+                    break;
+                case 'AZURE_BLOB_SAS':
+                    scanType = ScanJobAccessSourceEnum.AzureBlobSas;
+                    break;
+                case 'AZURE_BLOB_AD':
+                    scanType = ScanJobAccessSourceEnum.AzureBlobAd;
+                    break;
+                case 'GOOGLE_SIGNED':
+                    scanType = ScanJobAccessSourceEnum.GoogleSigned;
+                    break;
+                case 'GOOGLE_OAUTH':
+                    scanType = ScanJobAccessSourceEnum.GoogleOauth;
+                    break;
+                default:
+                    throw new Error(`Unsupported community scan type: ${communityScan}`);
+            }
+            
+            // For community scan types other than Hugging Face, require model version
+            if (communityScan !== 'HUGGING_FACE' && !version) {
+                throw new Error('When running a community scan other than a Hugging Face model, you must provide a model version.');
+            }
+            
+            results = await client.modelScanner.communityScan(modelName, modelPath, scanType, version);
         } else {
-            results = await client.modelScanner.scanFile(modelName, modelPath);
+            // Handle local file/folder scanning
+            const stats = fs.statSync(modelPath);
+            if (stats.isDirectory()) {
+                results = await client.modelScanner.scanFolder(modelName, modelPath);
+            } else {
+                results = await client.modelScanner.scanFile(modelName, modelPath);
+            }
         }
+        
         const anyDetected = await hasDetections(results);
         if (anyDetected) {
             const taskResult = failOnDetections ? tl.TaskResult.Failed : tl.TaskResult.SucceededWithIssues;
