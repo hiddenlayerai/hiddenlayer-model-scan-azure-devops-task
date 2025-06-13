e4c6e119d1816ec51aca444fccab81b7a1c62614
adrummond-hl <143145478+adrummond-hl@users.noreply.github.com>
2025-06-13 13:36:24 -0600

    Int 361 community scan support (#147)
    * INT-89 implement community scan support
    
    * Add tests and gitignore debug files
    
    * Update readme

---
+++ task/tests/community_scan_success_without_fail_on_detections.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+import tmrm = require('azure-pipelines-task-lib/mock-run');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+import path = require('path');
+import * as process from 'process';
+
+const taskPath = path.join(__dirname, '..', 'index.js');
+const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
+
+console.log(__dirname);
+tmr.setInput('modelPath', 'ScanMe/Models');
+tmr.setInput('apiUrl', process.env['HL_API_URL'] || 'https://api.us.hiddenlayer.ai');
+tmr.setInput('hlClientID', process.env['HL_CLIENT_ID'] || '');
+tmr.setInput('hlClientSecret', process.env['HL_CLIENT_SECRET'] || '');
+tmr.setInput('failOnDetections', 'false');
+tmr.setInput('sarifFile', __dirname + '/results/results_community_scan_success_without_fail_on_detections.sarif');
+tmr.setInput('communityScan', 'HUGGING_FACE');
+tmr.setInput('modelVersion', 'main');
+
+tmr.run(); 
